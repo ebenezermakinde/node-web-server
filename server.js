3351df6e0dcf249a5f29577ed9c4bf09c9b9ce17
ebenezermakinde <ebenezermakinde@yahoo.co.uk>
2019-01-21 13:29:42 +0100

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs =  require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -56,6 +57,6 @@ app.get('/bad', (req, res) => {
   res.send({errorMessage: 'Page was not found!'});
 });
 
-app.listen(3000, () => {
-  console.log('App is listening on port 3000');
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
 })
