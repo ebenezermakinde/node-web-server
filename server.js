236b59afdcbaeaac3da72a9b600a53b0afbcf088
ebenezermakinde <ebenezermakinde@yahoo.co.uk>
2019-01-21 02:19:18 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs =  require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance', {
+//     pageTitle: 'Maintenance Page',
+//     maintenanceMessage: 'The site is currently being maintained. We\'ll be back soon'
+//   });
+// });
+
+app.use(express.static(__dirname +'/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  // res.send('Hello there!');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to iamObey.com'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({errorMessage: 'Page was not found!'});
+});
+
+app.listen(3000, () => {
+  console.log('App is listening on port 3000');
+})
